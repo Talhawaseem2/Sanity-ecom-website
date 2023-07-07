529a303108444ecce5596680356a7f7fce0f96ed
Talhawaseem2 <114868886+Talhawaseem2@users.noreply.github.com>
2023-07-07 10:44:32 +0500

    some changes

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -3,6 +3,7 @@ import { oneProductType } from '../utils/ProductsDataArrayAndType'
 import Image from 'next/image'
 import  ImageUrlBuilder  from "@sanity/image-url";
 import { client } from '../../../sanity/lib/client';
+// import PortableText from 'react-portable-text';
 
 const builder = ImageUrlBuilder(client);
 
@@ -19,11 +20,15 @@ const Card: FC<{singleProductData: oneProductType}> = ({singleProductData}) => {
     
       
   return (
-    <div className='border-4'>
-        <div className='w-96'>
-            <Image height={500} width={500} src={urlFor(singleProductData.image[0]).width(500).url()} alt={singleProductData.image[0].alt} />         
+    <div className='border-4 max-w-sm min-w-[24rem] space-y-3'>
+        <div className='w-full'>
+            <Image height={1000} width={1000} src={urlFor(singleProductData.image[0]).width(500).url()} alt={singleProductData.image[0].alt} />         
         </div>
-        <div></div>
+        <div className='space-y-1 text-gray-600 font-semibold text-lg'>
+          <h6>{singleProductData.productName}</h6>
+          {/* <PortableText className='text-sm font-normal' content={singleProductData.description} /> */}
+          <p>${singleProductData.price}</p>
+        </div>
     </div>
   )
 }
--- src/components/views/ProductCarousel/index.tsx
+++ src/components/views/ProductCarousel/index.tsx
@@ -1,19 +1,69 @@
 "use client"
 import { oneProductType } from "@/components/utils/ProductsDataArrayAndType";
-import { FC } from "react"
+import { Component, FC, ReactNode } from "react"
 import Card from "../Card";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
 
+// const ProductCarousel: FC<{ProductData: Array<oneProductType>}> = ({ProductData}: any) => {
+//   return (
+//     <div className="flex">
+//       {ProductData.map((item:oneProductType, index:number)=>(
+//         <Card singleProductData={item}/>
+//       ))
+//       }
+//     </div>
+//   )
+// }
 
-const ProductCarousel: FC<{ProductData: Array<oneProductType>}> = ({ProductData}: any) => {
-  let dataToSend = ProductData.slice(0,3)
-  return (
-    <div className="flex">
-      {dataToSend.map((item:oneProductType, index:number)=>(
-        <Card key={index} singleProductData={item}/>
+// export default ProductCarousel
+
+export default class ProductCarousel extends Component<{ProductData : Array<oneProductType>}> {
+  getVal =() => {
+    console.log(this.props.ProductData);
+  }
+  render(): ReactNode{
+    const settings = {
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+    return(
+        <Slider {...settings}>
+       {this.props.ProductData.map((item:oneProductType, index:number)=>(
+        <Card singleProductData={item}/>
       ))
-      }
-    </div>
-  )
-}
 
-export default ProductCarousel
+       }
+        </Slider>
+    )
+
+  }
+}
