9bc7770ba41fe08090ba693c173979e880b96d52
Talhawaseem2 <114868886+Talhawaseem2@users.noreply.github.com>
2023-07-07 10:48:49 +0500

    resolving

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -20,7 +20,7 @@ const Card: FC<{singleProductData: oneProductType}> = ({singleProductData}) => {
     
       
   return (
-    <div className='border-4 max-w-sm min-w-[24rem] space-y-3'>
+    <div className='max-w-sm min-w-[24rem] space-y-3'>
         <div className='w-full'>
             <Image height={1000} width={1000} src={urlFor(singleProductData.image[0]).width(500).url()} alt={singleProductData.image[0].alt} />         
         </div>
