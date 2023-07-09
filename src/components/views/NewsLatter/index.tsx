ccfa8bae9179b72c62abbddb980d7e06adf448d7
Talhawaseem2 <114868886+Talhawaseem2@users.noreply.github.com>
2023-07-09 06:58:46 +0500

    responsive newslatter

--- src/components/views/NewsLatter/index.tsx
+++ src/components/views/NewsLatter/index.tsx
@@ -1,7 +1,8 @@
 
 const Newslatter = () => {
   return (
-    <div className="text-center space-y-4 h-[60vh] flex flex-col justify-center items-center text-gray-800">
+    <div className="relative text-center space-y-4 h-[60vh] flex flex-col justify-center items-center text-gray-800">
+      <h6 className="absolute lg:text-9xl text-7xl font-bold text-[#F2F3F7] text-center -z-50">Newsletter</h6>
       <h6 className="text-4xl md:text-4xl font-bold">Subscribe Our Newsletter</h6>
       <p className="max-w-[15rem] font-normal">Get the latest information and promo offers directly</p>
       <div className="flex gap-4 flex-wrap items-center justify-center">
